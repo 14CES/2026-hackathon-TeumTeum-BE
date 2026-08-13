2089954b889bc95cf41cd625530908ecadc467f6
14CES <2025111968@dgu.ac.kr>
2026-08-13 16:03:02 +0900

    feat : 메인 화면 조회 및 메인 틈 시간 설정 구현

--- main/main/settings.py
+++ main/main/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-
-    'rest_framework',
     
     'accounts',
     'magazines',
@@ -129,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Email
--- main/teumteum/urls.py
+++ main/teumteum/urls.py
@@ -1,5 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from .views import *
+from rest_framework import routers
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = "teumteum"
+
+default_router = routers.SimpleRouter(trailing_slash=False)
+default_router.register("main", MainViewSet, basename="main")
 
 urlpatterns = [
-]
+    path("", include(default_router.urls)),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
